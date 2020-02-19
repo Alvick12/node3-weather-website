4f869bfb6ba044211d081b987ea79b1c72f47e8b
Vasken Serpekian <vserpekian@collabralink.com>
2020-02-19 14:18:52 -0500

    Updating forcast with data and bug fixes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,17 +5,17 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/33e2f0c558f3ba6fd5858052397c30c7/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
 
-    request({ url, json: true}, (error, {body: data}) => {
+    request({ url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service', undefined)
-        } else if(data.error) {
+        } else if(body.error) {
             callback('Unable to find location', undefined)
         } else { 
-            const dailyTemp = data.daily.temperature
-            const dailyPrecipProbability = data.daily.precipProbability
-            const dailyTempHigh = data.daily.temperatureMax
-            const dailyTempLow = data.daily.temperatureMin
-            const dataSummary = data.daily.data[0].summary + ' It is currently ' + dailyTemp + ' degrees out. The high for today is ' +  
+            const dailyTemp = body.currently.temperature
+            const dailyPrecipProbability = Math.round(body.daily.data[0].precipProbability * 100)
+            const dailyTempHigh = body.daily.data[0].temperatureHigh
+            const dailyTempLow = body.daily.data[0].temperatureLow
+            const dataSummary = body.daily.data[0].summary + ' It is currently ' + dailyTemp + ' degrees out. The high for today is ' +  
                 dailyTempHigh + ' and the low is ' + dailyTempLow + '. There is a ' + dailyPrecipProbability + '% chance of rain.'
             callback(undefined, dataSummary)
         }
